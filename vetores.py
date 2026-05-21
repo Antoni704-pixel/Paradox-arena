250fd1aabcbc61a44e2ffaf051783cfb381f3527
Antoni704-pixel <antoniofstedile@gmail.com>
2026-05-20 21:31:10 -0300

    aula 20/05

---
+++ vetores.py
@@ -0,0 +1,17 @@
+# Um vetor é uma variavel que recebe diversos valores, em posiçoes diferentes
+alunos=["davi","daniel","wanchise","igor","ian","nataasha","fabio"]
+print("formação original")
+print(alunos
+#Para adicionar um valor ao final da lista, usamos a função append
+alunos.append("Antoni")]"]
+#para remover um valor da lista, usamos a função remove
+alunos.remove("nicoli")
+print("removemos a nicoli")
+print()
+#usamos a função len para saber quantos elementos tem a lista
+print(f"há {len(alunos)} presentes hoje")
+print()
+#para iterar um elemento entre os itens do vetor, podemos usar função
+#for <variavel> in <vetor>:
+for aluno in alunos:
+    print(f"boa noite {aluno}")
